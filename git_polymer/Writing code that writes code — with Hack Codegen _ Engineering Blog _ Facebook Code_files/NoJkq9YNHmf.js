f4caf4717fa719f279109e001e2fa7727db2d60f
jfrazzano <jfrazzano@gmail.com>
2015-09-06 11:54:01 -0400

    This appears to be needed???

---
+++ git_polymer/Writing code that writes code — with Hack Codegen _ Engineering Blog _ Facebook Code_files/NoJkq9YNHmf.js
@@ -0,0 +1,5 @@
+/*!CK:2765630845!*//*1439230875,*/
+
+if (self.CavalryLogger) { CavalryLogger.start_js(["ohBeJ"]); }
+
+__d('Engsite',['Event','CSS','cx'],function a(b,c,d,e,f,g,h,i,j){if(c.__markCompiled)c.__markCompiled();var k=function(){if(window.location.hash||window.pageYOffset)return;if(document.height<window.outerHeight)document.body.style.height=window.outerHeight+50+'px';setTimeout(function(){window.scrollTo(0,1);},50);},l={hideAddressBar:function(){if(navigator.userAgent.match(/android|iphone/i)){k();window.addEventListener('orientationchange',k);}},showHide:function(m,n){h.listen(m,'click',function(){i.toggleClass(n,"_3v5e");});}};f.exports=l;},null);
